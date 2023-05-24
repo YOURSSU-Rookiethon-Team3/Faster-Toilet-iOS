14659e71f644516602f56812461176598fdc213d
White-Patrasche <5338095@gmail.com>
2023-05-24 11:31:18 +0900

    feat: color extension

---
+++ FasterToilet/Utils/Colors.swift
@@ -0,0 +1,20 @@
+//
+//  Colors.swift
+//  FasterToilet
+//
+//  Created by 심상현 on 2023/05/24.
+//
+
+import SwiftUI
+
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue: Double(hex & 0xFF) / 255,
+            opacity: alpha
+        )
+    }
+}
