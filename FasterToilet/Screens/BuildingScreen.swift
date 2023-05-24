153e528cb9be10a62fd28fa4330a137c94644160
White-Patrasche <5338095@gmail.com>
2023-05-24 11:32:34 +0900

    feat: temp building screen

--- FasterToilet/Screens/BuildingScreen.swift
+++ FasterToilet/Screens/BuildingScreen.swift
@@ -7,17 +7,50 @@
 
 import SwiftUI
 
-struct BuildingScreen:View {
+struct BuildingScreen: View {
     func filterButton() -> some View {
-        VStack {
-            Image(systemName: "minus")
+        Button {
+            
+        } label: {
+            VStack {
+                Image(systemName: "slider.horizontal.3")
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black)
+                Text("화장실 조건 추가")
+                    .font(.caption2)
+                    .foregroundColor(.black)
+            }
         }
     }
     
+    
     var body: some View{
-        VStack {
-            filterButton()
-            Image(systemName: "plus")
+        VStack(alignment: .leading) {
+            ZStack(alignment: .center) {
+                VStack {
+                    HStack {
+                        Spacer()
+                        filterButton()
+                    }
+                    Rectangle()
+                        .foregroundColor(.blue)
+                        .frame(width: 100, height: 100)
+                    Text("어떤 화장실로 가고싶나요?")
+                        .font(.title2)
+                    Text("가고싶은 건물을 클릭해주세요")
+                        .font(.subheadline)
+                    
+                }
+            }.frame(height: 200)
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.cyan)
+                    .frame(width: UIScreen.main.bounds.width, height: 400)
+                Button{} label: {
+                    Text("화장실 조건 추가")
+                }
+            }
+            Spacer()
         }
         .navigationBarHidden(true)
     }
