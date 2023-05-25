017827f3db20284f19ed949759f33d8a595938f8
White-Patrasche <5338095@gmail.com>
2023-05-25 15:04:06 +0900

    feat: color data set

---
+++ FasterToilet/Consts/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  FasterToilet
+//
+//  Created by 심상현 on 2023/05/25.
+//
+
+import SwiftUI
+
+let primaryColor = Color(0x1650A7)
+let textColor = Color(0x000000)
+let highlightColor = Color(0x2B4CBF)
+let subtextColor = Color(0x3A3D40)
+
+let infoIconColor = Color(0x2D4568)
+let statusActiveColor = Color(0x0064BF)
+let statusInactiveColor = Color(0x7E7E7E)
+let starActiveColor = Color(0xEB5151)
+let starInactiveColor = Color(0x969696)
+let basicButtonColor = Color(0x5A5A5A)
