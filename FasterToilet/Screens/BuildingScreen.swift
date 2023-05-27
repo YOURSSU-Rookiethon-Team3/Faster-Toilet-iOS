164236dc86831086c28fc65bd2035a3750e52c57
White-Patrasche <5338095@gmail.com>
2023-05-28 02:23:13 +0900

    feat: give buildling data with API call

--- FasterToilet/Screens/BuildingScreen.swift
+++ FasterToilet/Screens/BuildingScreen.swift
@@ -6,8 +6,22 @@
 //
 
 import SwiftUI
+import Moya
 
 struct BuildingScreen: View {
+    var building:Building
+    var restrooms:[Restroom] = []
+    @State var buildings:[Building] = []
+    
+    init() {
+        restrooms.append(Restroom.init(id: 50,floor: 4, location: "엘리베이터옆", isMale: true, rating: 5, congestion: 3, vanity: true, bidet: false, disabled: false, extra: "무야호", buildings: [], createdAt: "15465"))
+        restrooms.append(Restroom.init(id: 40,floor: 4, location: "엘리베이터옆", isMale: true, rating: 5, congestion: 3, vanity: true, bidet: false, disabled: false, extra: "무야호", buildings: [], createdAt: "15465"))
+        restrooms.append(Restroom.init(id: 20,floor: 4, location: "엘리베이터옆", isMale: true, rating: 5, congestion: 3, vanity: true, bidet: false, disabled: false, extra: "무야호", buildings: [], createdAt: "15465"))
+        restrooms.append(Restroom.init(id: 10,floor: 4, location: "엘리베이터옆", isMale: true, rating: 5, congestion: 3, vanity: true, bidet: false, disabled: false, extra: "무야호", buildings: [], createdAt: "15465"))
+        building = Building.init(id: 1547, name: "정보과학관",floors: [-1,1,2,3,4] , restrooms: restrooms)
+        
+        
+    }
     
     var body: some View{
         VStack {
@@ -31,12 +45,32 @@ struct BuildingScreen: View {
                     }
                 }
 
-                NavigationLink(destination: DetailScreen()) {
+                NavigationLink(destination: DetailScreen(
+                building: building)) {
                     CustomButton(title: "디테일 뷰")
                 }
             }
             Spacer()
         }
         .navigationBarHidden(true)
+        .onAppear {
+                    let buildingService = BuildingService()
+                    buildingService.getBuildings { [self] result in
+                        switch result {
+                        case .success(let buildingsResponse):
+                            buildings = buildingsResponse.buildings
+                            print(buildingsResponse.buildings)
+                        case .failure(let error):
+                            print("Error: \(error)")
+                        }
+                    }
+                }
+    }
+}
+
+
+struct BuildingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        BuildingScreen()
     }
 }
