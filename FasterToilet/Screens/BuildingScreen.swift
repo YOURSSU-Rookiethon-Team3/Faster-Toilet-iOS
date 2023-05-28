3d7070f08d5bf0c5cd24311b1dfcebd03f0e72e9
hyeonju <shj040201@naver.com>
2023-05-29 00:04:40 +0900

    빌딩 스크린

--- FasterToilet/Screens/BuildingScreen.swift
+++ FasterToilet/Screens/BuildingScreen.swift
@@ -13,18 +13,109 @@ struct BuildingScreen: View {
     
     
     var body: some View{
+        GeometryReader {geometry in
         VStack {
             VStack {
                 Image("toilet").resizable()
-                    .frame(width: 80, height: 85)
+                    .frame(width: geometry.size.width * 0.21,height: geometry.size.height * 0.12)
                 Text("어떤 화장실로 가고싶나요?")
                     .font(.title2)
                 Text("가고싶은 건물을 클릭해주세요")
                     .font(.subheadline)
-            }.padding(.bottom, 10)
+            }
+        
             ZStack {
                 Image("building_map")
                     .resizable()
+                    .frame(width: geometry.size.width)
+                
+                VStack(spacing: geometry.size.height * 0.05) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.black, lineWidth: 1)
+                            .background(Color.white.opacity(0.5))
+                            .frame(width: 145, height: 112)
+                            .offset(x: 100, y:-60)
+                        Text("중앙도서관")
+                            .offset(x: 100, y: -90)
+                        HStack{
+                            Image("star")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x:105, y:-70)
+                            Text("4.0")
+                                .offset(x:100, y:-70)
+                            Text("/")
+                                .offset(x:100, y:-70)
+                            Image("stoilet")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x:100, y:-70)
+                            Text("4.0")
+                                .offset(x:95, y:-70)
+                            
+                            
+                        }
+                        CustomButton1(title: "이동하기   ")
+                            .offset(x:100, y:-40)
+                    }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.black, lineWidth: 1)
+                            .background(Color.white.opacity(0.5))
+                            .frame(width: 145, height: 112)
+                            .offset(x: -30, y: -50)
+                        Text("형남공학관")
+                            .offset(x: -30, y: -80)
+                        HStack{
+                            Image("star")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x:-25, y:-60)
+                            Text("4.0")
+                                .offset(x:-30, y:-60)
+                            Text("/")
+                                .offset(x:-30, y:-60)
+                            Image("stoilet")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x:-30, y:-60)
+                            Text("4.0")
+                                .offset(x:-35, y:-60)
+                        }
+                        CustomButton2(title: "이동하기   ")
+                            .offset(x:-30, y:-30)
+                    }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 12)
+                            .stroke(Color.black, lineWidth: 1)
+                            .background(Color.white.opacity(0.5))
+                            .frame(width: 145, height: 112)
+                            .offset(x: 80, y: 0)
+                        Text("정보과학관")
+                            .offset(x: 80, y: -30)
+                        HStack{
+                            Image("star")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x:85, y:-10)
+                            Text("4.0")
+                                .offset(x:80, y:-10)
+                            Text("/")
+                                .offset(x:80, y:-10)
+                            Image("stoilet")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x:80, y:-10)
+                            Text("4.0")
+                                .offset(x:75, y:-10)
+                        }
+                        CustomButton3(title: "이동하기   ")
+                            .offset(x:80, y:20)
+                    }
+                }
+                .frame(width: geometry.size.width)
+                
                 
                 VStack {
                     Spacer()
@@ -32,11 +123,12 @@ struct BuildingScreen: View {
                         CustomButton(title: "화장실 조건 추가")
                     }
                 }
+            }
                 
-                NavigationLink(destination: DetailScreen(
-                building: buildings[0])) {
-                    CustomButton(title: "디테일 뷰")
-                }
+//                NavigationLink(destination: DetailScreen(
+//                building: buildings[0])) {
+//                    CustomButton(title: "디테일 뷰")
+//                }
             }
             Spacer()
         }
@@ -57,6 +149,74 @@ struct BuildingScreen: View {
     }
 }
 
+
+struct CustomButton0: View {
+    var title: String
+    
+    var body: some View {
+        Button(action: {
+            // 버튼이 클릭되었을 때 수행할 액션
+        }) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(20)
+        }
+    }
+}
+struct CustomButton1: View {
+    var title: String
+    
+    var body: some View {
+        Button(action: {
+            // 버튼이 클릭되었을 때 수행할 액션
+        }) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .frame(width: 100, height: 30)
+                .cornerRadius(20)
+        }
+    }
+}
+struct CustomButton2: View {
+    var title: String
+    
+    var body: some View {
+        Button(action: {
+            // 버튼이 클릭되었을 때 수행할 액션
+        }) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .frame(width: 100, height: 30)
+                .cornerRadius(20)
+        }
+    }
+}
+struct CustomButton3: View {
+    var title: String
+    
+    var body: some View {
+        Button(action: {
+            // 버튼이 클릭되었을 때 수행할 액션
+        }) {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .frame(width: 100, height: 30)
+                .cornerRadius(20)
+        }
+    }
+}
 
 struct BuildingScreen_Previews: PreviewProvider {
     static var previews: some View {
